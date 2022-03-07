a21835f09a58d03597dbe140e81736e953bc36fe
Str0uk <83171911+Str0uk@users.noreply.github.com>
2022-03-07 12:45:56 +0300

    Update csvreader.cpp

--- csvreader.cpp
+++ csvreader.cpp
@@ -39,30 +39,3 @@ std::vector<Employe> CSVReader::readAll(){
                }
             return result;
 }
-
-
-std::vector<std::string> CSVReader::split(const std::string& str, char delim){
-    std::vector<std::string> tokens;
-
-    int len = str.length();
-
-    if (!str.empty())
-    {
-        string buff = "";
-        for (int i = 0; i < len; i++) {
-
-            if (str[i]==delim) {
-                tokens.push_back(buff);
-                buff = "";
-            }
-            else {
-                buff += str[i];
-            }
-
-        };
-        tokens.push_back(buff); //так как в конце строчки нет разделителя
-
-    }
-
-    return tokens;
-}
